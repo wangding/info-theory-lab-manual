9d06484c019a9e3dcb2fa30713a6068078cc2012
wangding <408542507@qq.com>
2020-08-27 14:55:57 +0800

    docs: add cdn

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,28 @@ module.exports = function (grunt) {
         }]
       }
     },
+    replace: {
+      html: {
+        src: ['_book/*.html'],
+        overwrite: true,
+        replacements: [{
+          from: 'href="gitbook',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/info-theory-lab-manual@gh-pages/gitbook'
+        }, {
+          from: 'href="styles',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/info-theory-lab-manual@gh-pages/styles'
+        }, {
+          from: 'href="images',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/info-theory-lab-manual@gh-pages/images'
+        }, {
+          from: 'src="gitbook',
+          to: 'src="https://cdn.jsdelivr.net/gh/wangding/info-theory-lab-manual@gh-pages/gitbook'
+        }, {
+          from: 'src="images',
+          to: 'src="https://cdn.jsdelivr.net/gh/wangding/info-theory-lab-manual@gh-pages/images'
+        }]
+      }
+    },
     copy: {
       html: {
         files: [
@@ -56,6 +78,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-text-replace');
 
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
+  grunt.registerTask('minify', ['replace', 'htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
 };
